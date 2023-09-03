01d7d8fd43fc0566a8854d2e853f0462656b476a
Wafairuzzaki <143861156+Wafairuzzaki@users.noreply.github.com>
2023-09-03 16:35:43 +0700

    Add files via upload
    Penugasan NIM genap (menghitung bangun ruang) plotting B. Nomor 2

---
+++ genaptugas2.py
@@ -0,0 +1,10 @@
+# Input data tabung
+π = float (3.14)
+jari_jari = int (input("Masukan jarijari tabung : "))
+tinggi = int (input("Masukan tinggi tabung : "))
+
+# Menghitung volume tabung
+volume = π * jari_jari * jari_jari * tinggi
+
+# Menampilkan hasil volume tabung
+print("Volume balok adalah :", volume)
