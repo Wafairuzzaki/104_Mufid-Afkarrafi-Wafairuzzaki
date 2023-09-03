7550b40e049fb8268eaf197abcf43f08d5e680ab
Wafairuzzaki <143861156+Wafairuzzaki@users.noreply.github.com>
2023-09-03 16:19:15 +0700

    Add files via upload
    Penugasan NIM genap (menghitung bangun ruang) plotting B. Nomor 1

---
+++ genaptugas1.py
@@ -0,0 +1,10 @@
+# Input data balok
+panjang = int (10)
+lebar = int (7)
+tinggi = int (5)
+
+# Menghitung volume balok 
+volume = panjang * lebar * tinggi
+
+# Menampilkan hasil volume balok
+print("Volume balok adalah :", volume) 
